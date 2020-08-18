b70af21bcadbea32b3f2b3df9b9c69074c380264
Vittorio Erlapso Banfi <erlapso@users.noreply.github.com>
2020-08-17 18:26:55 -0700

    deploy

--- handler.js
+++ handler.js
@@ -2,11 +2,14 @@ const { prop } = require('ramda/src')
 const Botsociety = require('botsociety')
 const csv = require('csv-generate')
 
-const triggerExport = async(event) => {
+const triggerExport = async (event) => {
+  console.log('starting');
   const headers = prop('headers', event)
   const accessToken = prop('Access-Token', headers)
   const { user_id, api_key_public } = headers
   const body = JSON.parse(prop('body', event))
+  console.log(body)
+  console.log(headers)
 
   const http = require('https')
   /*
@@ -19,74 +22,131 @@ const triggerExport = async(event) => {
       callbackUrl,
     }
   */
-  var exportCSV = function() {
-    r  = new Promise(function(done, error) {
-      var botsociety = new Botsociety({userId: headers.user_id, apiKey: headers.api_key_public})
-      botsociety.getConversation(body.designId)
-      .then(function(conversation) {
-        var CSVfile = "messageId,sender,intent,content"
-        for (i = 0; i < conversation.messages.length; i++) { //loop the messages
-          var message = conversation.messages[i]
-          CSVfile += "\n"
-          CSVfile += message.id + "," + message.sender_id + ","
-          for (y = 0; y < message.attachments.length; y++ ) { //loop the attachments
-            var attachment = message.attachments[y]
-            for (z = 0; z < attachment.utterances.length; z++ ) { //loop the utterances
-              CSVfile += JSON.stringify(attachment.utterances[z]) + "," //content of the first utterance
+  var getConversation = (conversationId, config) => {
+    httpOptions = {
+      hostname: config.domain,
+      port: 443,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'user_id': `${config.userId}`,
+        'api_key_public': `${config.apiKey}`,
+      },
+      path: `/designs/${conversationId}/integrations`
+    }
+    var promise = new Promise(function (done, error) {
+      var responseData = ""
+      const req = http.get(httpOptions, (res) => {
+        console.log(`STATUS getconv: ${res.statusCode}`);
+        // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          responseData += chunk
+        });
+        res.on('end', () => {
+          //console.log(responseData)
+          if (res.statusCode != 200) {
+            return error(JSON.parse(responseData))
+          } else {
+            return done(JSON.parse(responseData))
+          }
+        });
+      });
+      req.on('error', (e) => {
+        console.log('returning error on getConv')
+        error(e)
+      });
+      req.end();
+    })
+    return promise;
+  }
+  var exportCSV = function () {
+    r = new Promise(function (done, error) {
+      console.log("GETTING CONV")
+      getConversation(body.designId, { userId: headers.user_id, apiKey: headers.api_key_public, domain: body.domain })
+        .then(function (conversation) {
+          var CSVfile = "messageId,sender,intent,content"
+          for (i = 0; i < conversation.messages.length; i++) { //loop the messages
+            var message = conversation.messages[i]
+            CSVfile += "\n"
+            CSVfile += message.id + "," + message.sender_id + ","
+            for (y = 0; y < message.attachments.length; y++) { //loop the attachments
+              var attachment = message.attachments[y]
+              for (z = 0; z < attachment.utterances.length; z++) { //loop the utterances
+                CSVfile += JSON.stringify(attachment.utterances[z]) + "," //content of the first utterance
+              }
             }
           }
-        }
-        done(CSVfile)
-      })
+          done(CSVfile)
+        })
+        .catch(function (err) {
+          console.log('error exporting CSV')
+          error(err)
+        })
     })
     return r;
   }
-  exportCSV()
-  .then(function(base64_file) {
-    let buff = new Buffer(base64_file);
-    let base64data = buff.toString('base64');
-    console.log("CSV created")
-    httpOptions = {
-      hostname: "app.botsociety.io",
-      port: 443,
-      method: 'POST',
-      path: body.callbackUrl,
-      headers: {
-        'Content-Type': 'application/json',
-        'user_id': `${headers.user_id}`,
-        'api_key_public': `${headers.api_key_public}`,
-      }
-    };
-    var postData = JSON.stringify({
-      file: base64data,
-      file_name: 'export.csv',
-      type: 'file_download'
-    })
-    var responseData = "";
-    const req = http.request(httpOptions, (res) => {
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
-        responseData += chunk
-      });
-      res.on('end', () => {
-        console.log("CSV sent")
-      });
-    });
-    req.on('error', (e) => {
-      console.error(e)
-    });
-    req.write(postData);
-    req.end();
-  })
+  const main = new Promise(function (resolve, reject) {
+    exportCSV()
+      .then(function (base64_file) {
+        let buff = new Buffer(base64_file);
+        let base64data = buff.toString('base64');
+        console.log("CSV created")
+        httpOptions = {
+          hostname: body.domain,
+          port: 443,
+          method: 'POST',
+          path: body.callbackUrl,
+          headers: {
+            'Content-Type': 'application/json',
+            'user_id': `${headers.user_id}`,
+            'api_key_public': `${headers.api_key_public}`,
+          }
+        };
+        //console.log(httpOptions)
+        var postData = JSON.stringify({
+          file: base64data,
+          file_name: 'export.csv',
+          type: 'file_download'
+        })
+        var responseData = "";
+        const req = http.request(httpOptions, (res) => {
+          res.setEncoding('utf8');
+          res.on('data', (chunk) => {
+            responseData += chunk
+          });
+          res.on('end', () => {
+            console.log("CSV sent")
+            var statusCode = 200;
+            var info = { message: "The export succeded" };
+            resolve({
+              statusCode,
+              body: JSON.stringify({
+                info,
+              }),
+            })
+          });
+        });
+        req.on('error', (e) => {
+          console.log(e)
+          //throw new Error(e)
+        });
+        req.write(postData);
+        req.end();
 
-  var statusCode = 200;
-  var info = {message: "The export succeded"};
-  return {
-    statusCode,
-    body: JSON.stringify({
-      info,
-    }),
-  }
+      })
+      .catch(function (err) {
+        console.log("error")
+        console.log(err)
+        reject({
+          statusCode: 500,
+          body: JSON.stringify({
+            err,
+          }),
+        })
+      })
+  })
+  return main;
 }
 
 module.exports = {
--- test/triggerExport copy.js
+++ test/triggerExport copy.js
@@ -1,43 +1,45 @@
 require('dotenv').config()
 var http = require('http')
-describe.skip("Trigger an export", function () {
-  it("Exports a design from the localhost, as if the user clicked on the Build mode button", function (done) {
+describe("Trigger an export in staging", function () {
+  it("Exports a design from the localhost, as if the user clicked on the Build mode button in staging", function (done) {
+    this.timeout(80000)
     httpOptions = {
       hostname: "localhost",
       port: 5000,
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'user_id': process.env.USER_ID,
-        'api_key_public': process.env.API_KEY,
+        'user_id': '5b17b69d747e93000c4dce34',
+        'api_key_public': 'f43032bcadd94167b78428cff2c2c5c7',
       },
       path: `/dev/export`
     }
-    console.log(httpOptions)
     var responseData = ""
     var postData = {
-      domain: process.env.DOMAIN,
+      domain: "staging.botsociety.io",
       deviceToExport: undefined,
       welcomePathId: process.env.WELCOME_PATH_ID,
-      designId: process.env.CONVERSATION_ID,
+      designId: '5f3b073bfbee1a5d5d87ad9f',
       callbackUrl: '/customIntegrations/exportCompleted'
     }
     const req = http.request(httpOptions, (res) => {
-      console.log(`STATUS: ${res.statusCode}`);
-      if (res.statusCode != 200) {
-        //return done(res.statusCode)
-      }
       console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
       res.setEncoding('utf8');
       res.on('data', (chunk) => {
         responseData += chunk
       });
-      res.on('end', () => {
+      res.on('end', (d) => {
         if (!res.complete) {
           done(new Error('The connection was terminated while the message was still being sent'))
         } else {
-          //console.log(responseData) // Log your response here
-          done()
+          console.log(`STATUS: ${res.statusCode}`);
+          if (res.statusCode != 200) {
+            //return done(res.statusCode)
+            console.log(res.info)
+            //done(res.statusCode)
+          } else {
+            done()
+          }
         }
       });
     });
@@ -45,6 +47,7 @@ describe.skip("Trigger an export", function () {
     req.on('error', (e) => {
       console.log("There was an error in the request")
       console.log(e)
+      //done(e)
     });
     req.end();
   })
